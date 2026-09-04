451bea062a0caecb5f6c1177cf6d04bb1f1d72b2
Moinuddin Patel <moinuddinpatel846@gmail.com>
2026-09-04 19:18:38 +0530

    checked authlayout

--- client/src/layouts/AdminLayout.jsx
+++ client/src/layouts/AdminLayout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Sidebar } from '../components/admin/Sidebar';
 import { Header } from '../components/admin/Header';
